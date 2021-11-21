901e158744873da737a34beed9e02af53ca448e1
Vyas Raina <sayvaniar@gmail.com>
2021-11-21 19:02:49 +0000

    nn module super class

--- models.py
+++ models.py
@@ -5,7 +5,7 @@ import torchvision.transforms as transforms
 
 class Classifier(nn.Module):
     def __init__(self, arch, num_classes, size=32) -> None:
-
+        super().__init__()
         self.normalize = transforms.Normalize(mean=[0.5071, 0.4865, 0.4409], std=[0.2009, 0.1984, 0.2023])
         self.main_model =  make_model(arch, num_classes=num_classes, pretrained=True, input_size=(size, size))
     
