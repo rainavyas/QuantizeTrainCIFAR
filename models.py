924bf2b323bcf964aa0ab838954765e3d3bf4354
Vyas Raina <sayvaniar@gmail.com>
2021-11-22 01:19:23 +0000

    debug

--- models.py
+++ models.py
@@ -6,7 +6,7 @@ import torchvision.transforms as transforms
 class Classifier(nn.Module):
     def __init__(self, arch, num_classes, size=32) -> None:
         super().__init__()
-        self.normalize = transforms.Normalize(mean=[0.5071, 0.4865, 0.4409], std=[0.2009, 0.1984, 0.2023])
+        # self.normalize = transforms.Normalize(mean=[0.5071, 0.4865, 0.4409], std=[0.2009, 0.1984, 0.2023])
         self.main_model =  make_model(arch, num_classes=num_classes, pretrained=True, input_size=(size, size))
     
     def forward(self, X):
@@ -14,5 +14,6 @@ class Classifier(nn.Module):
         X: torch Tensor
                 [B x C x H x W]
         '''
-        return self.main_model(self.normalize(X))
+        return self.main_model(X)
+        # return self.main_model(self.normalize(X))
 
